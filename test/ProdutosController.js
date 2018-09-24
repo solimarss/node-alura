428ab4ba6b996b0c401a766ced2ff79af9cea1bf
Solimar Silva <solimar.silva@trt8.jus.br>
2018-09-24 15:06:34 -0300

    testes

--- test/ProdutosController.js
+++ test/ProdutosController.js
@@ -1,24 +1,24 @@
 //Para rodar os testes: node_modules/mocha/bin/mocha
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
+describe('ProdutosController',function(){
+    it('listagem json',function(done){
+        request.get('/produtos')
+        .set('Accept','application/json')
+        .expect('Content-Type',/json/)
+        .expect(200,done);
+    });
 
-describe('#ProdutosController',function(){
-
-    it('#listagem json',function(done){
-        var configuracoes = {
-            hostname: 'localhost',
-            port:3000,
-            path:'/produtos',
-            headers: {
-                'Accept' : 'application/json'
-            }
-        };
-
-        http.get(configuracoes,function(res){
-            assert.equal(res.statusCode,200);
-            assert.equal(res.headers['content-type'],'application/json; charset=utf-8');
-            done();
-        });       
+    it('#cadastro de novo produto com dados invalidos', function(done){
+        request.post('/produtos')
+            .send({titulo: "", descricao: "novo livro"})
+            .expect(400, done);
+    });
+    
+    it('#cadastro de novo produto com dados validos', function(done){
+        request.post('/produtos')
+        .send({titulo:"titulo",descricao:"novo livro",preco:20.50})
+        .expect(302,done);
     });
 });
