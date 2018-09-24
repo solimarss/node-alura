4daf0cde1dbe052f2312e104c1a7eba7ee003e9f
Solimar Silva <solimar.silva@trt8.jus.br>
2018-09-24 14:39:23 -0300

    hgh

--- test/ProdutosController.js
+++ test/ProdutosController.js
@@ -1,7 +1,26 @@
 //Para rodar os testes: node_modules/mocha/bin/mocha
-describe('#ProdutosController', function(){
-    it('#listagem json',function(){
-        console.log("teste de verificacao de listagem json");
-    });
+var http = require('http');
+describe('#ProdutosController',function(){
 
+    it('#listagem json',function(done){
+        var configuracoes = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept' : 'application/json'
+            }
+        };
+
+        http.get(configuracoes,function(res){
+            if(res.statusCode == 200){
+                console.log("Status ta ok");
+            }
+
+            if(res.headers['content-type'] == 'application/json;charset=utf-8'){
+                console.log("Content type ok");
+            }
+            done();
+        });       
+    });
 });
